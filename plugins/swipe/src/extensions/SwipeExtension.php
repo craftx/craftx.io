134790858c05a5ba1abb9e927da31a3339c522ec
Selvin Ortiz <selvin@selvin.co>
2017-05-02 17:28:44 -0500

    Updates dependencies and newsletter subscription related pages

--- plugins/swipe/src/extensions/SwipeExtension.php
+++ plugins/swipe/src/extensions/SwipeExtension.php
@@ -7,51 +7,84 @@ use Craft;
 use selvinortiz\swipe\Swipe;
 use function selvinortiz\swipe\swipe;
 
-class SwipeExtension extends \Twig_Extension {
-
+class SwipeExtension extends \Twig_Extension
+{
+    const ERROR_FLASH_KEY  = '__error';
     const NOTICE_FLASH_KEY = '__notification';
 
     private $_methods = [
+        'setError',
+        'hasError',
+        'getError',
         'setNotice',
         'hasNotice',
-        'getNotice'
+        'getNotice',
     ];
 
-    public function getName() {
+    public function getName()
+    {
         return Swipe::t('Swipe Extension');
     }
 
-    public function getFilters() {
+    public function getFilters()
+    {
         $filters = [];
-        foreach ($this->_methods as $method) {
+        foreach ($this->_methods as $method)
+        {
             $filters[] = new \Twig_SimpleFilter($method, [$this, $method]);
         }
 
         return $filters;
     }
 
-    public function getFunctions() {
+    public function getFunctions()
+    {
         $functions = [];
-        foreach ($this->_methods as $method) {
+        foreach ($this->_methods as $method)
+        {
             $functions[] = new \Twig_SimpleFunction($method, [$this, $method]);
         }
 
         return $functions;
     }
 
-    public function setNotice(string $message) {
-        if ($message === null) {
+    public function setError(string $message)
+    {
+        if ($message === null)
+        {
+            return Craft::$app->session->getFlash(self::ERROR_FLASH_KEY);
+        }
+
+        Craft::$app->session->setFlash(self::ERROR_FLASH_KEY, Swipe::t($message));
+    }
+
+    public function hasError()
+    {
+        return Craft::$app->session->hasFlash(self::ERROR_FLASH_KEY);
+    }
+
+    public function getError()
+    {
+        return Craft::$app->session->getFlash(self::ERROR_FLASH_KEY);
+    }
+
+    public function setNotice(string $message)
+    {
+        if ($message === null)
+        {
             return Craft::$app->session->getFlash(self::NOTICE_FLASH_KEY);
         }
 
         Craft::$app->session->setFlash(self::NOTICE_FLASH_KEY, Swipe::t($message));
     }
 
-    public function hasNotice() {
+    public function hasNotice()
+    {
         return Craft::$app->session->hasFlash(self::NOTICE_FLASH_KEY);
     }
 
-    public function getNotice() {
+    public function getNotice()
+    {
         return Craft::$app->session->getFlash(self::NOTICE_FLASH_KEY);
     }
 }
