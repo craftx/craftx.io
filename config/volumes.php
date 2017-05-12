ff548f6377af43b550aa0c59ce24370588930456
Selvin Ortiz <selvin@selvin.co>
2017-05-12 13:25:11 -0500

    Massive update to get closer to production deployment 😉

---
+++ config/volumes.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    // Production
+    '*' => [
+        'hangouts' => [
+            'url' => 'https://craftx.io/assets/hangouts/',
+            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+        ]
+    ],
+    // Staging
+    'dev.' => [
+        'hangouts' => [
+            'url' => 'https://dev.craftx.io/assets/hangouts/',
+            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+        ]
+    ],
+    // Local
+    '.dev' => [
+        'hangouts' => [
+            'url' => 'http://craftx.dev/assets/hangouts/',
+            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+        ]
+    ]
+];
