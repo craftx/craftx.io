6d2878bb156c277f93d1c5fab2d43324dc576caa
Selvin Ortiz <selvin@selvin.co>
2017-05-15 22:42:36 -0500

    Fixes critical issue with volume paths

--- config/volumes.php
+++ config/volumes.php
@@ -4,21 +4,21 @@ return [
     '*' => [
         'hangouts' => [
             'url' => 'https://craftx.io/assets/hangouts/',
-            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+            'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
         ]
     ],
     // Staging
     'dev.' => [
         'hangouts' => [
             'url' => 'https://dev.craftx.io/assets/hangouts/',
-            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+            'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
         ]
     ],
     // Local
     '.dev' => [
         'hangouts' => [
             'url' => 'http://craftx.dev/assets/hangouts/',
-            'path' => dirname(__DIR__, 2).'/web/assets/hangouts/'
+            'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
         ]
     ]
 ];
