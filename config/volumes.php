1cbace911354e3be4a99d12df8d476601bdd77be
Selvin Ortiz <selvin@selvin.co>
2017-05-17 12:26:16 -0500

    Updates volume configs for all envrionments

--- config/volumes.php
+++ config/volumes.php
@@ -2,23 +2,35 @@
 return [
     // Production
     '*' => [
+        'blog' => [
+            'url' => 'https://craftx.io/assets/blog/',
+            'path' => dirname(__DIR__, 1).'/web/assets/blog/'
+        ],
         'hangouts' => [
             'url' => 'https://craftx.io/assets/hangouts/',
             'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
-        ]
+        ],
     ],
     // Staging
     'dev.' => [
+        'blog' => [
+            'url' => 'https://dev.craftx.io/assets/blog/',
+            'path' => dirname(__DIR__, 1).'/web/assets/blog/'
+        ],
         'hangouts' => [
             'url' => 'https://dev.craftx.io/assets/hangouts/',
             'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
-        ]
+        ],
     ],
     // Local
     '.dev' => [
+        'blog' => [
+            'url' => 'http://craftx.dev/assets/blog/',
+            'path' => dirname(__DIR__, 1).'/web/assets/blog/'
+        ],
         'hangouts' => [
             'url' => 'http://craftx.dev/assets/hangouts/',
             'path' => dirname(__DIR__, 1).'/web/assets/hangouts/'
-        ]
+        ],
     ]
 ];
