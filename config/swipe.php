b9ea4b44b88857b4078478a28dff2b9fe4ce38b5
Selvin Ortiz <selvin@selvin.co>
2018-08-31 11:38:22 -0500

    Update config for username disallow list

--- config/swipe.php
+++ config/swipe.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'stripeSecretKey' => getenv('SWIPE_STRIPE_SECRET_KEY'),
-    'stripePublicKey' => getenv('SWIPE_STRIPE_PUBLIC_KEY'),
-    'usernameBlacklist' => include 'swipe_username_blacklist.php'
+    'stripeSecretKey'   => getenv('SWIPE_STRIPE_SECRET_KEY'),
+    'stripePublicKey'   => getenv('SWIPE_STRIPE_PUBLIC_KEY'),
+    'usernameBlacklist' => include 'swipe-username-disallow.php',
 ];
